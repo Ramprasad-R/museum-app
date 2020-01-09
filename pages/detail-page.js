1237925db660454efb2d91243deb633867817f98
Ramprasad-R <ramprasad.info@gmail.com>
2020-01-09 15:33:16 +0100

    update ths js to get name and massage and add to HTML element

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,3 +1,15 @@
 function submitComment(){
-    console.log("Hello");
+    const getName = document.getElementById("name");
+    const name = getName.value;
+    const getMessage = document.getElementById("message");
+    const message = getMessage.value;
+    const commentDiv = document.createElement('div');
+    const commentH2 = document.createElement('h4');
+    const commentP = document.createElement('p');
+    commentH2.innerText = `${name} said:`;
+    commentP.innerText = message;
+    commentDiv.classList.add("section");
+    commentDiv.appendChild(commentH2);
+    commentDiv.appendChild(commentP);
+    console.log(commentDiv);
 }
